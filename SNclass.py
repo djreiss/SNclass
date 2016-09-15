1ec558efa21acdd6108774c45fc1fdaa6cafc5e7
David Reiss <reiss@uw.edu>
2016-09-14 17:14:03 -0700

    move functions to SNclass module

--- SNclass.py
+++ SNclass.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
-plt.style.use('ggplot')
-import subprocess
-import pickle
-import glob
 import gzip
+
+import george
+from george import kernels
+from scipy.optimize import minimize
+
+filters = [b'g', b'r', b'i', b'z']  # , b'Y']
+
 
 def SNPhotCC_Parser(filename):
     '''
@@ -53,30 +56,30 @@ def SNPhotCC_Parser(filename):
                 sim_z = float(s[1])
             elif s[0] == 'VARLIST':
                 break
-    obs = pd.read_table(gzip.GzipFile(filename), sep='\s+', header=0, skiprows=lineno, skipfooter=1, 
+    obs = pd.read_table(gzip.GzipFile(filename), sep='\s+', header=0, skiprows=lineno, skipfooter=1,
                         comment='DETECTION', engine='python',
                         usecols=[1,2,4,5,6,7,8,9])
-    metadata = {'survey': survey, 'snid': snid, 'sim_type': sim_type, 'sim_z': sim_z, 'ra': ra, 'decl': decl, 
+    metadata = {'survey': survey, 'snid': snid, 'sn_type': sn_type, 'sim_type': sim_type,
+                'sim_z': sim_z, 'ra': ra, 'decl': decl,
                 'mwebv': mwebv, 'hostid': hostid, 'hostz': hostz, 'spec': spec}
     return obs, metadata
 
-import george
-from george import kernels
-from scipy.optimize import minimize
-filters = [b'g', b'r', b'i', b'z'] #, b'Y']
 
-def SNPhot_fitter_filt(obs, filt=b'r', verbose=False):
+def SNPhot_fitter_filt(obs, filt=b'r', verbose=False, kernelMultiplier=5.):
+    fname = None
     if isinstance(obs, str):  # assume it's a filename
+        fname = obs
         obs, metadata = SNPhotCC_Parser(obs)
 
     df = obs[obs.FLT == filt]
+    #print fname, filt, df.shape
     x = df.MJD.values
     y = df.FLUXCAL.values
     dy = df.FLUXCALERR.values
-    
-    kernel = np.var(y) * kernels.ExpSquaredKernel(100)
+
+    kernel = kernelMultiplier * np.var(y) * kernels.ExpSquaredKernel(100.)
     gp = george.GP(kernel)
-    gp.compute(x, dy*2.)
+    gp.compute(x, np.abs(dy))
 
     if verbose:
         print("Initial ln-likelihood: {0:.2f}".format(gp.lnlikelihood(y)))
@@ -98,25 +101,44 @@ def SNPhot_fitter_filt(obs, filt=b'r', verbose=False):
         print("\nFinal ln-likelihood: {0:.2f}".format(gp.lnlikelihood(y)))
     return gp
 
-def SNPhot_fitter(obs, verbose=False):
-    if isinstance(obs, str):  # assume it's a filename
-        obs, metadata = SNPhotCC_Parser(obs)
 
-    gps = [SNPhot_fitter_filt(obs, filt, verbose=verbose) for filt in filters]
-    return gps
-    
+def SNPhot_fitter(obs, verbose=False):
+    metadata = fname = None
+    if isinstance(obs, str):  # assume it's a filename
+        fname = obs
+        obs, metadata = SNPhotCC_Parser(obs)
+        metadata['filename'] = fname
+
+    gps = {}
+    for filt in filters:
+        try:
+            gps[filt] = SNPhot_fitter_filt(obs, filt, verbose=verbose)
+        except:
+            try:
+                print 'ERROR(1): %s %s -- trying again' % (fname, filt)
+                gps[filt] = SNPhot_fitter_filt(obs, filt, verbose=verbose, kernelMultiplier=10.)
+            except:
+                try:
+                    print 'ERROR(2): %s %s -- trying again' % (fname, filt)
+                    gps[filt] = SNPhot_fitter_filt(obs, filt, verbose=verbose, kernelMultiplier=25.)
+                except:
+                    print 'ERROR FATAL: %s %s' % (fname, filt)
+    return obs, metadata, gps
+
+
 def SNPhot_plotter_filt(obs, gp, filt=b'r'):
     df = obs[obs.FLT == filt]
     x = df.MJD.values
     y = df.FLUXCAL.values
     dy = df.FLUXCALERR.values
 
-    x_pred = np.linspace(x.min()-100., x.max()+100., 1000)
-    pred, pred_var = gp.predict(y, x_pred, return_var=True)
-    #pred, pred_cov = gp.predict(y, x_pred, return_cov=True)
-    
-    plt.fill_between(x_pred, pred - np.sqrt(pred_var), pred + np.sqrt(pred_var), color="k", alpha=0.2)
-    plt.plot(x_pred, pred, "k", lw=1.5, alpha=0.5)
+    if gp is not None:
+        x_pred = np.linspace(x.min()-100., x.max()+100., 1000)
+        pred, pred_var = gp.predict(y, x_pred, return_var=True)
+        #pred, pred_cov = gp.predict(y, x_pred, return_cov=True)
+        plt.fill_between(x_pred, pred - np.sqrt(pred_var), pred + np.sqrt(pred_var), color="k", alpha=0.2)
+        plt.plot(x_pred, pred, "k", lw=1.5, alpha=0.5)
+
     plt.errorbar(x, y, yerr=dy, fmt=".k", capsize=0)
     plt.ylim((y-dy*2.).min(), (y+dy*2.).max())
     plt.xlim(x.min()-30, x.max()+30)
@@ -124,7 +146,12 @@ def SNPhot_plotter_filt(obs, gp, filt=b'r'):
     return plt
 
 def SNPhot_plotter(obs, gps):
+    if gps is None:
+        gps = dict(zip(filters, [None, None, None, None]))
     for i, filt in enumerate(filters):
         plt.subplot(2, 2, i+1)
-        SNPhot_plotter_filt(obs, gps[i], filt)
+        gp = None
+        if filt in gps:
+            gp = gps[filt]
+        SNPhot_plotter_filt(obs, gp, filt)
 
